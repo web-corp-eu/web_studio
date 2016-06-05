e33202682eaa4782bbf05f3a98437be4f2fd4bc8
web-corp-eu <web.corp.eu.gmail.com>
2016-06-05 15:02:43 +0300

    sidebar

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8" />
+
+	<title></title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/main.css" />
+	<link rel="stylesheet" href="css/media.css" />
+	<link rel="stylesheet" href="css/fonts.css" />
+
+</head>
+
+<body>
+ <!-- Full Page Image Background Carousel Header -->
+			<header>
+			<div class="banner">
+			  <div class="container">
+			    <div id="nav-icon" class="col-lg-2" data-effect="st-effect-1">
+					  <span></span>
+					  <span></span>
+					  <span></span>
+					  <span></span>
+					  <span></span>
+					  <span></span>
+			   	</div>
+
+
+
+		<div class="menu">
+      <ul id="list-menu">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Help</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+
+    <!-- Main body -->
+
+
+				<div class="logo_main col-lg-4 col-lg-offset-2">
+							<a href=""><img src="img/logo.png"></a>
+			  </div>
+			      <nav class="navbar col-lg-3 col-lg-offset-2 mgt70">
+			          <ul class="nav nav-pills ">
+			            <li><a href="" class="biliboard"><span>About</span></a></li>
+			            <li><a href="" class="biliboard"><span>Portfolio</span></a></li>
+			            <li><a href="" class="biliboard"><span>Blog</span></a></li>
+			          </ul>
+			      </nav>
+			   </div>
+			</div>
+			</header>
+ <div id="myCarousel" class="carousel slide">
+		 <!-- Indicators -->
+		 <ol class="carousel-indicators">
+				 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				 <li data-target="#myCarousel" data-slide-to="1"></li>
+				 <li data-target="#myCarousel" data-slide-to="2"></li>
+		 </ol>
+
+		 <!-- Wrapper for Slides -->
+		 <div class="carousel-inner">
+
+				 <div class="item active">
+						 <!-- Set the first background image using inline CSS below. -->
+						 <div class="fill" style="background-image:url('img/first_bg.jpg');"></div>
+						 <div class="carousel-caption">
+								 <h2>First slider</h2>
+						 </div>
+				 </div>
+
+				 <div class="item">
+						 <!-- Set the second background image using inline CSS below. -->
+						 <div class="fill" style="background-image:url('img/second_bg.jpg');"></div>
+						 <div class="carousel-caption">
+								 <h2>Second slider</h2>
+						 </div>
+				 </div>
+
+				 <div class="item">
+						 <!-- Set the third background image using inline CSS below. -->
+						 <div class="fill" style="background-image:url('img/third_bg.jpg');"></div>
+						 <div class="carousel-caption">
+								 <h2>Third slider</h2>
+						 </div>
+				 </div>
+		 </div>
+
+		 <!-- Controls -->
+		 <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				 <span class="icon-prev"></span>
+		 </a>
+		 <a class="right carousel-control" href="#myCarousel" data-slide="next">
+				 <span class="icon-next"></span>
+		 </a>
+</div>
+<div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+<div class="row">
+            <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading ">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+            <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+             <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+             <div class="col-6 col-sm-6 col-lg-3 panel-info">
+              <div class="panel-heading">Heading</div>
+              <div class="panel-body">
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
+            </div><!--/span-->
+          </div>    </div>
+
+
+	<footer>
+	</footer>
+
+	<script src="libs/jquery/jquery-2.1.4.min.js"></script>
+	<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+	<script src="js/main.js."></script>
+	<script src="js/common.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,23 @@
+// NAV BT3
+
+$(document).ready(function(){
+	//sidebar menu
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+		if ($(this).hasClass('open')){
+			$('.menu').animate({
+				left: "0px"
+			}, 200);
+			$('body').animate({
+				left: "285px"
+			}, 200);
+		} else {
+			$('.menu').animate({
+	      left: "-285px"
+	    }, 200);
+	    $('body').animate({
+	      left: "0px"
+	    }, 200);
+		}
+	});
+});
