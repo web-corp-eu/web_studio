38f505e22d7fbff3cb0f948e013cd3e38b57ee55
web-corp <web.corp.eu@gmail.com>
2016-06-06 13:31:34 +0300

    ;

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 
 	<meta charset="utf-8" />
 
-	<title></title>
+	<title>Web Corp</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-
+    
+    <link rel="shortcut icon" href="img/sm_logo.png" type="image/png">
 	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
@@ -22,14 +23,15 @@
 			<header>
 			<div class="banner">
 			  <div class="container">
-			    <div id="nav-icon" class="col-lg-2" data-effect="st-effect-1">
-					  <span></span>
-					  <span></span>
-					  <span></span>
-					  <span></span>
-					  <span></span>
-					  <span></span>
-			   	</div>
+			    <div id="nb-close-toggle" class="">
+                  <div class="top"></div>
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
+                    <path class="circle" fill="none" stroke-width="2" stroke-miterlimit="10" d="M16,32h32c0,0,11.723-0.306,10.75-11
+                    c-0.25-2.75-1.644-4.971-2.869-7.151C50.728,7.08,42.767,2.569,33.733,2.054C33.159,2.033,32.599,2,32,2C15.432,2,2,15.432,2,32
+                    c0,16.566,13.432,30,30,30c16.566,0,30-13.434,30-30C62,15.5,48.5,2,32,2S1.875,15.5,1.875,32"></path>
+                    </svg>
+                  <div class="bottom"></div>
+                </div>
 
 
 
@@ -103,7 +105,15 @@
 		 </a>
 </div>
 <div class="container marketing">
-
+<!------------------------------------------------------------------------------------------------------->	
+  <div class="block col-lg-12">
+  <div class="pensil">
+  <span></span>
+  <i class="fa fa-pencil fa-3x"></i>
+  </div>
+      <p>Our services</p>
+  </div>
+<!------------------------------------------------------------------------------------------------------->
       <!-- Three columns of text below the carousel -->
 <div class="row">
             <div class="col-6 col-sm-6 col-lg-3 panel-info">
@@ -162,8 +172,86 @@
                   <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
             </div>
             </div><!--/span-->
-          </div>    </div>
-
+          </div>    
+        </div>
+<!------------------------------------------------------------------------------------------------------->	
+  <div class="block col-lg-12">
+  <div class="pensil">
+  <span></span>
+  <i class="fa fa-pencil fa-3x"></i>
+  </div>
+  <p>Our packages</p>
+  </div>
+<!------------------------------------------------------------------------------------------------------->	
+<div class="pricing-tables block">
+    <ul class="pricing-table col-lg-3">
+      <li class="title">Basic</li>
+      <li class="price">$150</li>
+      <li class="description">Logo only</li>
+      <li>Round I: 50 sketches</li>
+      <li>Round II: 4 unique ideas</li>
+      <li>Round III: 2 refined</li>
+      <li>Round IV: 1 finalized</li>
+      <li class="deliverables">Delivery: AI</li>
+      <li class="cta-button"><a href="#" class="button">Buy Now</a></li>
+    </ul>
+    <ul class="pricing-table col-lg-3">
+      <li class="title">Business</li>
+      <li class="price">$250</li>
+      <li class="description">Logo + Business card</li>
+      <li>Round I: 100 sketches</li>
+      <li>Round II: 6 unique ideas</li>
+      <li>Round III: 3 refined</li>
+      <li>Round IV: 1 finalized</li>
+      <li class="deliverables">Delivery: AI, EPS</li>
+      <li class="cta-button"><a href="#" class="button">Buy Now</a></li>
+    </ul>
+    <ul class="pricing-table col-lg-3">
+      <li class="title">Identity</li>
+      <li class="price">$350</li>
+      <li class="description">Logo + Identity</li>
+      <li>Round I: All Rounds</li>
+      <li>+ Business cards</li>
+      <li>+ Letterhead</li>
+      <li>+ SNS covers (Facebook)</li>
+      <li class="deliverables">Delivery: AI, EPS, SVG</li>
+      <li class="cta-button"><a href="#" class="button">Buy Now</a></li>
+    </ul>
+    <ul class="pricing-table col-lg-3">
+      <li class="title">Identity</li>
+      <li class="price">$350</li>
+      <li class="description">Logo + Identity</li>
+      <li>Round I: All Rounds</li>
+      <li>+ Business cards</li>
+      <li>+ Letterhead</li>
+      <li>+ SNS covers (Facebook)</li>
+      <li class="deliverables">Delivery: AI, EPS, SVG</li>
+      <li class="cta-button"><a href="#" class="button">Buy Now</a></li>
+    </ul>
+</div>
+<!------------------------------------------------------------------------------------------------------->	
+  <div class="block col-lg-12">
+  <div class="pensil">
+  <span></span>
+  <i class="fa fa-pencil fa-3x"></i>
+  </div>
+      <p>Latest Projects</p>
+  </div>
+<!------------------------------------------------------------------------------------------------------->
+ <div class="container">
+    <ul id="imageGallery">
+        <li>
+            <a href=""><img id="thumb" src="" alt=""></a> 
+        </li>
+        <li>
+            <a href=""><img id="thumb" src="" alt=""></a>
+        </li>
+        <li>
+            <a href=""><img id="thumb" src="" alt=""></a>
+        </li>
+    </ul>
+ 
+ </div>
 
 	<footer>
 	</footer>
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // NAV BT3
 $(document).ready(function(){
 	//sidebar menu
-	$('#nav-icon').click(function(){
+	$('#nb-close-toggle').click(function(){        
 		$(this).toggleClass('open');
 		if ($(this).hasClass('open')){
 			$(this).css('position','fixed');
@@ -20,5 +20,29 @@ $(document).ready(function(){
 	      left: "0px"
 	    }, 500);
 		}
-	});
+	});    
+    
+      $('.pricing-table:nth-child(3)').addClass('pop-out'); 
+
+      $('.pricing-table').hover( 
+        function() {
+          if ( $(this).hasClass('pop-out') ) {
+            /* do nothing */
+          } else {
+            $('.pricing-table').removeClass('pop-out');
+            $(this).addClass('pop-out');
+          }
+        }, function() {
+          $(this).removeClass('pop-out');
+          $('.pricing-table:nth-child(3)').addClass('pop-out');
+        }
+      );
+    
+    var $btn = document.getElementById('nb-close-toggle');
+
+    $btn.addEventListener('click',function(e){
+      this.classList.toggle('is-open');
+      this.classList.toggle('is-closed');
+
+    });
 });
